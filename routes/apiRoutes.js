46b3dd54093d11fb6736039809d73e595a51e24d
Megan <carnagh5@gmail.com>
2021-01-14 15:21:07 -0500

    Added GET route to apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,1 +1,16 @@
+// Require Dependencies
+const db = require("../models");
 
+// Create api routes
+module.exports = (app) => {
+  // GET route to retrieve all workouts
+  app.get("/api/workouts", function (req, res) {
+    db.Workout.find({})
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  });
+};
