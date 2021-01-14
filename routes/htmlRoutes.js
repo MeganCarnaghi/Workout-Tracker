a0ba27021eb352c3c7d52bb2b67ed4c2f345114c
Megan <carnagh5@gmail.com>
2021-01-14 14:25:05 -0500

    set up html routes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,20 @@
+// Required Dependencies
+const path = require("path");
+
+// Create HTML Routes
+module.exports = (app) => {
+  // index.html route
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+
+  // exercise.html route
+  app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  });
+
+  //stats.html route
+  app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  });
+};
