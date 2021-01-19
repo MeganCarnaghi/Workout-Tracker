89b1fbb3fd5f31b6dcb1ccf49cd9dee70091c465
Megan <carnagh5@gmail.com>
2021-01-19 17:56:40 -0500

    Updating server.js

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ app.use(express.json());
 //Connect to the database
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false,
   useUnifiedTopology: true,
   useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 // HTTP request logger middleware
