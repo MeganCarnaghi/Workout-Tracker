485068202d723fb95cc98c71be438c8c89a1615b
Megan <carnagh5@gmail.com>
2021-01-19 17:35:15 -0500

    updated server.js

--- server.js
+++ server.js
@@ -18,11 +18,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Connect to the database
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+// });
+
+const MongoClient = require("mongodb").MongoClient;
+const uri =
+  "mongodb+srv://carnagh5:Spartan6@cluster0.krsxo.mongodb.net/workout?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
 });
 
 // HTTP request logger middleware
